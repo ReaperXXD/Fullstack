e58bfc3dbd05e5db778ca5708595322e8ccd2977
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-16 22:27:54 +0200

    mongoose connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Data base connected!!"))
+  .catch((err) => console.log(err));
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
