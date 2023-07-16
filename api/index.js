02ea541877ab47f824f174df86d5b0965062021a
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-16 22:57:58 +0200

    auth file updated

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 mongoose
@@ -11,6 +12,10 @@ mongoose
   })
   .then(() => console.log("Data base connected!!"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
