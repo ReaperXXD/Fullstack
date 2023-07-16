c549d6cf522ba9ec3d7dec5bfa7582ad75761191
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-16 23:53:04 +0200

    movie update route created

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -17,3 +17,24 @@ router.post("/", verify, async (req, res) => {
     res.status(403).json("You are not allowed!");
   }
 });
+
+//UPDATE
+
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
