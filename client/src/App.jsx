0c9fddf4399064561782ceb033dc15e51686d4c9
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-17 08:24:41 +0200

    material ui icons updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Home />;
+  return (
+    <div>
+      <Home />
+      <Register />
+      <Watch />
+      <Login />;
+    </div>
+  );
 };
 
 export default App;
--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,4 +1,4 @@
-import { ArrowBackOutlined } from "@material-ui/icons";
+import { ArrowBackOutlined } from "@mui/icons-material";
 import "./watch.scss";
 
 export default function Watch() {
